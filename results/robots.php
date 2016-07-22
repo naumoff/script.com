a415e0f425ed4a0306dad31608340fba3fe6f939
Andrey Naumoff <andrey.naumoff@gmail.com>
2016-07-22 22:24:23 +0300

    added Host quatity check, file size check of robots.txt, Sitemap availability check

--- results/robots.php
+++ results/robots.php
@@ -4,29 +4,62 @@ $file = $url.'/robots.txt';
 
 $handle = @fopen($file,'r');
 if(!$handle){
-	$mess1 =  'файл robots.txt - отсутствует!';
+	$mess1 = 'файл robots.txt - отсутствует!';
 	$warn1 = 'Программист: Создать файл robots.txt и разместить его на сайте!';
 }else{
 	$fileSize = remote_filesize($file);
 	$content = fread($handle,$fileSize);
 	setcookie('fileSize',$fileSize);
 	setcookie('content',$content);
-	$mess1 = "файл robots.txt присутствует"."\n";
-	$warn1 = "Доработки не требуются";
-	echo "<pre><a href='robots_read.php' target='_blank'>Read File</a>"."\n";
+	$mess1 = 'файл robots.txt присутствует.';
+	$warn1 = 'Доработки не требуются.';
+	echo "<pre><a href='robots_read.php' 
+                 target='_blank'>просмотр ROBOTS.TXT</a>"."\n";
 }
 
-echo "<a href='/index.php'>Back!</a>"."\n";
+echo "<a href='/index.php'>Вернуться к форме</a>"."\n";
 
-$pattern = "/^[^#]*host( )?:( )?/i";
-if (preg_match_all($pattern,$content,$matches,PREG_SET_ORDER)){
+$patternHost = "/^[^#]*host( )?:( )?/i";
+if (preg_match_all($patternHost,$content,$matches,PREG_SET_ORDER)){
 	$mess2 = 'Директива HOST указана.';
 	$warn2 = 'Доработки не требуются.';
 }else{
-	$mess2 = 'В файле robots.txt не указана директива Host';
-	$warn2 = 'Программист: Для того, чтобы поисковые системы знали, какая версия сайта является основных зеркалом, необходимо прописать адрес основного зеркала в директиве Host. В данный момент это не прописано. Необходимо добавить в файл robots.txt директиву Host. Директива Host задётся в файле 1 раз, после всех правил.';
+	$mess2 = 'В файле robots.txt не указана директива Host!';
+	$warn2 = 'Программист: Для того, чтобы поисковые системы знали, какая версия 
+	сайта является основных зеркалом, необходимо прописать адрес основного зеркала 
+	в директиве Host. В данный момент это не прописано. Необходимо добавить в файл
+	robots.txt директиву Host. Директива Host задётся в файле 1 раз, после всех 
+	правил!';
 }
 
+$hostCount = count($matches);
+if($hostCount != 1){
+	$mess3="В файле прописано {$hostCount} директив Host!";
+	$warn3='Программист: Директива Host должна быть указана в файле толоко 1 раз. Необходимо удалить все дополнительные директивы Host и оставить только 1, 
+	 корректную и соответствующую основному зеркалу сайта!';
+}else{
+	$mess3='В файле прописана 1 директива Host.';
+	$warn3='Доработки не требуются.';
+}
+
+$fileSizeKb = $fileSize / 1000;
+
+if($fileSizeKb <= 32){
+	$mess4="Размер файла robots.txt составляет {$fileSizeKb} кб, что находится в пределах допустимой нормы.";
+	$warn4='Доработки не требуются.';
+}else{
+	$mess4="Размер файла robots.txt составляет {$fileSizeKb} кб, что превышает допустимую норму!";
+	$warn4='Программист: Максимально допустимый размер файла robots.txt составляем 32 кб. Необходимо отредактировть файл robots.txt таким образом, чтобы его размер не превышал 32 Кб';
+}
+
+$patternSitemap = "/^[^#]*sitemap( )?:( )?/i";
+if (preg_match_all($patternSitemap,$content,$matches,PREG_SET_ORDER)){
+	$mess5 = 'Директива Sitemap указана.';
+	$warn5 = 'Доработки не требуются.';
+}else{
+	$mess5='В файле robots.txt не указана директива Sitemap';
+	$warn5='Программист: Добавить в файл robots.txt директиву Sitemap';
+}
 
 function remote_filesize($url) {
 	static $regex = '/^Content-Length: *+\K\d++$/im';
@@ -60,6 +93,18 @@ function remote_filesize($url) {
 		<tr>
 			<td><?php echo $mess2 ?></td>
 			<td><?php echo $warn2 ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $mess3 ?></td>
+			<td><?php echo $warn3 ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $mess4 ?></td>
+			<td><?php echo $warn4 ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $mess5 ?></td>
+			<td><?php echo $warn5 ?></td>
 		</tr>
 	</tbody>
 </table>
